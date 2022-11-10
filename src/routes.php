493371d8f54528ec4e9c531565157e329627fc9b
Macarub <joetsoso92@gmail.com>
2022-11-10 11:07:26 +0100

    creation_route

--- src/routes.php
+++ src/routes.php
@@ -7,5 +7,12 @@
 //          3. (optional) array of query string keys to send as parameter to the method
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
-    '' => ['HomeController', 'index',],
+    ''             => ['HomeController','index',],
+    'manga/add'    => ['MangaController','AddManga'],
+    'manga/delete' => ['MangaController','DeleteManga'] ['id'],
+    'manga/fav'    => ['MangaController','FavManga'],
+    'showcase'     => ['MangaController','Showindex'],
+    'library'      => ['MangaController','library'],
+    'category'     => ['MangaController','Category'] ['category'],
+    'show'         => ['MangaController','Show'] ['id'],
 ];
