220a97d7e9d4aacce385ac9b9238fa76d035ad0a
jeremy <jeremy.guimard.pro@gmail.com>
2022-11-12 17:42:20 +0100

    create first version showcase page

--- src/Controller/MangaController.php
+++ src/Controller/MangaController.php
@@ -1,1 +1,23 @@
 <?php
+
+namespace App\Controller;
+
+use App\Model\MangaManager;
+
+class MangaController extends AbstractController
+{
+    protected MangaManager $model;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->model = new MangaManager();
+    }
+
+    public function showcase(): string
+    {
+        $model = new MangaManager();
+        $mangas = $model->selectManga();
+        return $this->twig->render('Manga/showcase.html.twig', ['mangas' => $mangas]);
+    }
+}
--- src/Model/MangaManager.php
+++ src/Model/MangaManager.php
@@ -1,1 +1,23 @@
 <?php
+
+namespace App\Model;
+
+use App\Model\Connection;
+use PDO;
+
+class MangaManager extends AbstractManager
+{
+    protected PDO $pdo;
+
+    public function __construct()
+    {
+        $connection = new Connection();
+        $this->pdo = $connection->getconnection();
+    }
+
+    public function selectManga(): array
+    {
+        $statement = $this->pdo->query('SELECT title, image FROM manga');
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
