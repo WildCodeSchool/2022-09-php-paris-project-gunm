8c24b035072fa6aa441ef0ec0deda7a3b4227830
Melina Daghari <meldagh8@gmail.com>
2022-11-13 23:35:43 +0100

    create branch homepage, files html and css modified and add routing from homepage to showcase

---
+++ src/Controller/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+class UserController extends AbstractController
+{
+    public function login(): string
+    {
+        return $this->twig->render('User/login.html.twig');
+    }
+}
