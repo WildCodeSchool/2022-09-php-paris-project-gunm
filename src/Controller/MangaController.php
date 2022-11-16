ce2623b6cfc073b3701da5398e12a6f9a7945819
Cedric Nkene <cedric.nkn@gmail.com>
2022-11-16 23:04:58 +0100

    feature add manga done

--- src/Controller/MangaController.php
+++ src/Controller/MangaController.php
@@ -4,75 +4,86 @@ namespace App\Controller;
 
 use App\Controller\AbstractController;
 use App\Model\MangaManager;
+use DateTime;
 
 class MangaController extends AbstractController
 {
+    public function show()
+    {
+        return $this->twig->render('Manga/add.html.twig');
+    }
+
     public function add()
     {
+        $errors = [];
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $manga = array_map('trim', $_POST);
-            $errors = [];
 
-            if (empty(trim($_POST['title'])) && strlen($_POST['title']) >= 100) {
-                $errors[] = 'Le champ titre doit être rempli';
+            if (empty($manga['title']) || strlen($manga['title']) < 5 || strlen($manga['title']) > 100) {
+                $errors[] = 'Le champ titre est vide ou n\'a pas été correctement rempli';
             }
 
-            if (empty(trim($_POST['autor'])) && strlen($_POST['author']) >= 100) {
-                $errors[] = 'Le champ auteur doit être rempli';
+            if (empty(($manga['author'])) || strlen($manga['author']) < 5 || strlen($manga['author']) > 100) {
+                $errors[] = 'Le champ auteur est vide ou n\'a pas été correctement rempli';
             }
 
-            if (empty(trim($_POST['description']))) {
-                $errors[] = 'Le champ description doit être rempli';
+            if (empty($manga['description']) || strlen($manga['description']) > 1000) {
+                $errors[] = 'Le champ description est vide ou ne doit pas dépasser 1000 caractères';
             }
 
-            if (empty(trim($_POST['released_at']))) {
-                $errors[] = 'Le champ date de parution doit être rempli';
+            if (empty($manga['released_at']) || !$this->validateDate($manga['released_at'])) {
+                $errors[] = 'Le champ date est vide ou n\'a pas été correctement rempli';
             }
 
-            if (empty(trim($_POST['category'])) && strlen($_POST['category']) >= 100) {
-                $errors[] = 'Le champ categorie doit être rempli';
-            } elseif (empty($errors)) {
-                $mangaManager = new MangaManager();
-                $mangaManager->addManga($manga);
+            if (empty($manga['category']) || strlen($manga['category']) < 5 || strlen($manga['category']) > 100) {
+                $errors[] = 'Le champ categorie est vide ou n\'a pas été correctement rempli';
+            }
 
-                header('Location:/showcase/show?id=');
-            } else {
-                foreach ($errors as $error) {
-                    echo $error;
+            $errors = array_merge($errors, $this->checkImage());
+
+            if (empty($errors)) {
+                $fileName = basename($_FILES["image"]["name"]);
+                $filePath = $_SERVER['DOCUMENT_ROOT'] . '/../public/uploads/' . $fileName;
+
+                if (move_uploaded_file($_FILES["image"]["tmp_name"], $filePath)) {
+                    $manga['image'] = $filePath;
+                    $mangaManager = new MangaManager();
+                    $mangaManager->addManga($manga);
+
+                    header('Location: /');
                 }
             }
         }
+
+        return $this->twig->render('Manga/add.html.twig', [
+            'errors' => $errors
+        ]);
     }
 
-    public function addPicture()
+    private function checkImage()
     {
-        $mangaImage = array_map('trim', $_POST);
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $errors = [];
-            define('MAX_SIZE', 10000);
-            define('WIDTH_MAX', 800);
-            define('HEIGHT_MAX', 800);
-            define('EXTENSION', ['jpg','gif','png','jpeg']);
-
-            if (isset($_FILES['image']) && empty($_FILES['image'])) {
-                $errors[] = 'L\'image est obligatoire';
-            }
-        } elseif (
-            $_FILES['image'] > MAX_SIZE || $_FILES['image'] > WIDTH_MAX
-            || $_FILES['image'] > HEIGHT_MAX
-        ) {
-            $errors[] = 'La taille de l\'image est trop volumineuse';
-        } elseif (!in_array(EXTENSION, EXTENSION)) {
-            $errors[] = 'Le format n\'est pas accepté';
-        } elseif (empty($errors)) {
-            $mangaManager = new MangaManager();
-            $mangaManager->addManga($mangaImage);
-
-            header('Location:/showcase/show?id=');
+        $errors = [];
+        if (!isset($_FILES['image']) || $_FILES['image']['error'] === UPLOAD_ERR_NO_FILE) {
+            $errors[] = 'L\'image est obligatoire';
         } else {
-            foreach ($errors as $error) {
-                echo $error;
+            $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+
+            if ($_FILES['image']['size'] > MAX_SIZE_MANGA_IMAGE) {
+                $errors[] = 'La taille de l\'image est trop volumineuse';
+            }
+
+            if (!in_array($extension, ALLOWED_EXTENSIONS_MANGA_IMAGE)) {
+                $errors[] = 'Le format n\'est pas accepté';
             }
         }
+
+        return $errors;
+    }
+
+    private function validateDate($date, $format = 'Y-m-d'): bool
+    {
+        $dateFormat = DateTime::createFromFormat($format, $date);
+        return $dateFormat && $dateFormat->format($format) == $date;
     }
 }
