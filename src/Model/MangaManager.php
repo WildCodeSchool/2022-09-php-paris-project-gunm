e932edd9fec1c83f2ad8224603d356a348d97efb
jeremy <jeremy.guimard.pro@gmail.com>
2022-11-16 16:03:34 +0100

    createcategory filters

--- src/Model/MangaManager.php
+++ src/Model/MangaManager.php
@@ -34,10 +34,18 @@ class MangaManager extends AbstractManager
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function selectCategoryMangas(): array
-    {
-        $statement = $this->pdo->query('SELECT * FROM ' .static::TABLE. ' WHERE category LIKE %kodomo%');
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
+    // public function selectCategoryMangas(): array
+    // {
+    //     $statement = $this->pdo->query('SELECT * FROM ' .static::TABLE. ' WHERE category LIKE %kodomo%');
+    //     return $statement->fetchAll(PDO::FETCH_ASSOC);
+    // }
 
+    public function selectOneCategoryByCategory(string $category): array|false
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE category=:category");
+        $statement->bindValue(':category', $category, \PDO::PARAM_STR);
+        $statement->execute();
+        return $statement->fetch();
+    }
 }
